64d9579993b30b1b77bcd8bbed77e35e26b8e0c3
Jake-Hero <justinerainielramos@gmail.com>
2024-05-09 23:07:51 +0800

    Added Business Page & Re-written AJAX codes.

---
+++ account_header.php
@@ -0,0 +1,14 @@
+<div class="row mb-5">
+    <!-- Back to Character -->
+    <div class="col">
+        <a href="<?php echo SITE_URL; ?>/user/character.php?id=<?php echo $cid; ?>" class="btn btn-dark"><i class="fas fa-arrow-left"></i> Character</a>
+    </div>
+
+    <!--div class="col d-flex justify-content-end">
+        <div class="row">
+            <div class="col">
+                <a href="#" class="btn btn-dark"><i class="fas fa-cog"></i> Settings</a>
+            </div>
+        </div>
+    </div-->
+</div>
